f8309dde27572536a3d2931844eb6630989d8c61
Klaus Ma <klaus1982.cn@gmail.com>
2016-04-09 00:07:19 -0400

    Added Node.js app.

---
+++ kubernetes/tutorial/app/node/index.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var os = require('os')
+var mongoose = require('mongoose');
+
+var app = express()
+
+mongoose.connect('mongodb://localhost/mydb');
+
+var CntSchema = mongoose.Schema({
+  name: String,
+  val: String
+});
+
+var Cnt = mongoose.model('Cnt', CntSchema);
+
+var reqCnt = 0;
+
+app.get('/', function (req, res) {
+
+  Cnt.find({name: /cnt/}, function (err, c) {
+    if (err) console.log(err);
+    c.forEach(function(e){  
+      reqCnt = e.val;
+    })
+  });
+
+  res.send('The ' + reqCnt + ' request is handle by ' + os.hostname() + '.')
+
+  reqCnt = parseInt(reqCnt) + 1;
+
+  var cnt = new Cnt({ name: 'cnt', val: reqCnt });
+  cnt.save(function (err) {
+    if (err) console.log(err);
+  });
+})
+
+var server = app.listen(3001, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+  var cnt = new Cnt({ name: 'cnt', val: '1'});
+  cnt.save(function (err, c) {
+    if (err) console.log(err);
+  });
+})
