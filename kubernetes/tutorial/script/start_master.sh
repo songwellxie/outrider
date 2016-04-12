61e9b99107fa7d838f0b362b36438f13c707d442
Yong Feng <yongfeng@ca.ibm.com>
2016-04-11 22:26:13 -0400

    Added jenkins.

--- kubernetes/tutorial/script/start_master.sh
+++ kubernetes/tutorial/script/start_master.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SEC_TOP=/root/klaus/eos/kubernetes/tutorial/security
-
 killall -9 etcd kube-apiserver kube-controller-manager kube-scheduler kube-proxy docker flanneld
 kube-proxy --cleanup-iptables=true
 
@@ -28,14 +26,14 @@ ifconfig docker0 ${FLANNEL_SUBNET}
 nohup docker daemon --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU} >docker.log 2>&1 &
 
 nohup kube-apiserver --admission_control=ServiceAccount --allow-privileged=true \
-    --address=ma1demo1 --etcd-servers=http://ma1demo1:4001 \
+    --address=9.21.58.21 --etcd-servers=http://ma1demo1:4001 \
     --service-cluster-ip-range=10.1.0.0/16 --port=8888 2>&1 >api.log &
 
 nohup kube-controller-manager --master=http://ma1demo1:8888 \
     --service_account_private_key_file=/var/run/kubernetes/apiserver.key \
     --allocate-node-cidrs=true --cluster-cidr=10.1.0.0/16 2>&1 >ctrl.log &
 
-nohup kube-scheduler --address=ma1demo1 --master=http://ma1demo1:8888 2>&1 >sched.log &
+nohup kube-scheduler --address=9.21.58.21 --master=http://ma1demo1:8888 2>&1 >sched.log &
 
 # start kube-proxy
 nohup kube-proxy --master=http://ma1demo1:8888 >proxy.log 2>&1 &
