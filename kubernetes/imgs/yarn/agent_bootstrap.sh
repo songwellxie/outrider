4838682915f1f0b426d95eed25ebf59afb41e60b
root <root@cdemo.cn.ibm.com>
2016-06-04 17:39:30 +0800

    Add YARN dockerfile.

--- kubernetes/imgs/yarn/agent_bootstrap.sh
+++ kubernetes/imgs/yarn/agent_bootstrap.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+export JAVA_HOME=/usr
+
 HADOOP_PREFIX=/opt/hadoop-2.7.2
 HADOOP_YARN_HOME=$HADOOP_PREFIX
 HADOOP_CONF_DIR=/opt/hadoop-2.7.2/etc/hadoop
 
+sed "s/__HOSTNAME__/yarn_master/g" /root/core-site.xml.template > $HADOOP_CONF_DIR/core-site.xml
+sed "s/__HOSTNAME__/yarn_master/g" /root/yarn-site.xml.template > $HADOOP_CONF_DIR/yarn-site.xml
+sed "s/__HOSTNAME__/yarn_master/g" /root/hdfs-site.xml.template > $HADOOP_CONF_DIR/hdfs-site.xml
+
+mkdir /opt/hadoop272/dfs/name -p
+mkdir /opt/hadoop272/dfs/data -p
+
+$HADOOP_PREFIX/bin/hdfs namenode -format yarn_272_hdfs
+
 $HADOOP_YARN_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
 $HADOOP_PREFIX/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
 
+while true; do sleep 1000; done
--- kubernetes/imgs/yarn/master_bootstrap.sh
+++ kubernetes/imgs/yarn/master_bootstrap.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+export JAVA_HOME=/usr
+
 HADOOP_PREFIX=/opt/hadoop-2.7.2
 HADOOP_YARN_HOME=$HADOOP_PREFIX
 HADOOP_CONF_DIR=/opt/hadoop-2.7.2/etc/hadoop
+
+sed "s/__HOSTNAME__/"$(hostname)"/g" /root/core-site.xml.template > $HADOOP_CONF_DIR/core-site.xml
+sed "s/__HOSTNAME__/"$(hostname)"/g" /root/yarn-site.xml.template > $HADOOP_CONF_DIR/yarn-site.xml
+sed "s/__HOSTNAME__/"$(hostname)"/g" /root/hdfs-site.xml.template > $HADOOP_CONF_DIR/hdfs-site.xml
 
 mkdir /opt/hadoop272/dfs/name -p
 mkdir /opt/hadoop272/dfs/data -p
@@ -12,3 +18,4 @@ $HADOOP_PREFIX/bin/hdfs namenode -format yarn_272_hdfs
 $HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
 $HADOOP_PREFIX/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
 
+while true; do sleep 1000; done
