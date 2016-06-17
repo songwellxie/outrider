377608b02194f964dbe68af517cb02363dd17673
Klaus Ma <klaus1982.cn@gmail.com>
2016-06-17 11:02:50 +0800

    download Hadoop

--- kubernetes/imgs/yarn/build.sh
+++ kubernetes/imgs/yarn/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# wget http://apache.fayea.com/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz
+wget http://apache.fayea.com/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz
 
 docker build -t klaus1982/yarn_agent -f Dockerfile.agent .
 docker build -t klaus1982/yarn_master -f Dockerfile.master .
